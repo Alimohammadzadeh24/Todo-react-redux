eac1e04b5fb5badad9a2bd35be6e509021358c53
Ali-m24 <ali24mohammadzadeh@gmail.com>
2022-06-15 15:55:44 +0430

    add alert for empty input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { selelctTodoList } from "./features/todoSlice";
 import TodoItem from "./components/TodoItem";
 import { saveTodo } from "./features/todoSlice";
 import $ from "jquery";
+import Swal from 'sweetalert2'
 
 function App() {
   const TodoList = useSelector(selelctTodoList);
@@ -19,7 +20,13 @@ function App() {
   const addTask = () => {
     $("#TodoInput").val("");
     if (input === "" || input === undefined || input === null) {
-      console.log("There is nothing to add => Empty")
+      console.log("There is nothing to add => Empty");
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Your input is Empty!',
+        footer: 'Please add your Task in Input :)'
+      })
     } else {
       dispath(
         saveTodo({
